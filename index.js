f474e0ed1485160693bb613a71193191f6cd3947
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-07-09 13:49:04 -0600

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove(main);
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id ="victory"
+victory.innerHTML = "<h1>Sam is the champion!</h1>";
